4c051b644d7930b73726b3174c7f70e0283e6fb2
harryjcooper <harry@purplejamaudio.com>
2022-11-20 12:49:30 +0000

    possibly a bit b-tech

--- src/App.js
+++ src/App.js
@@ -9,9 +9,15 @@ const App = () => {
 		<>
 			<div className="absolute w-full h-full text-pink-200">
 				<div className="relative w-full h-full flex flex-col justify-end">
-					<h1 className="text-center font-bold text-5xl shadow-2xl">
+					<h2 className="text-center font-bold text-xs shadow-2xl text-opacity-20 pb-12">
 						OKAYILOVEYOUOKAYILOVEYOUOKAYILOVEYOU
-					</h1>
+					</h2>
+					<h2 className="text-center font-bold text-3xl shadow-2xl text-opacity-40 pb-12">
+						OKAYILOVEYOUOKAYILOVEYOUOKAYILOVEYOU
+					</h2>
+					<h2 className="text-center font-bold text-5xl shadow-2xl">
+						OKAYILOVEYOUOKAYILOVEYOUOKAYILOVEYOU
+					</h2>
 					<div className="absolute w-full font-bold text-5xl text-pink-300">
 						<h2 className="text-center pl-1 mb-custom-title">
 							OKAYILOVEYOUOKAYILOVEYOUOKAYILOVEYOU
